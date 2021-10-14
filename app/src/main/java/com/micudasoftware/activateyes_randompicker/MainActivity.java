fdf97865e5ba0b1315c9b943e7fd380e842d515e
DusanMicuda <dusanmicuda11@gmail.com>
2021-10-14 12:08:21 +0200

    creating layout files

--- app/src/main/java/com/micudasoftware/activateyes_randompicker/MainActivity.java
+++ app/src/main/java/com/micudasoftware/activateyes_randompicker/MainActivity.java
@@ -3,6 +3,8 @@ package com.micudasoftware.activateyes_randompicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LayoutInflater inflater = getLayoutInflater();
+        inflater.inflate(R.layout.button, (ViewGroup) findViewById(R.id.container));
+
+
     }
 }
