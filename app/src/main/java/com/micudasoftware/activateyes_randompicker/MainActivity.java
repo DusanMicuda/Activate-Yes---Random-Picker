5c85c722a0e6ed76452ba6f1d0756eb634af9d28
DusanMicuda <dusanmicuda11@gmail.com>
2021-10-17 18:28:06 +0200

    implemeneting get file

--- app/src/main/java/com/micudasoftware/activateyes_randompicker/MainActivity.java
+++ app/src/main/java/com/micudasoftware/activateyes_randompicker/MainActivity.java
@@ -1,25 +1,68 @@
 package com.micudasoftware.activateyes_randompicker;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    LayoutInflater layoutInflater;
+    ListView listView;
+    ViewGroup container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layoutInflater = getLayoutInflater();
+        listView = findViewById(R.id.list_view);
+        container = (ViewGroup) findViewById(R.id.container);
+        layoutInflater.inflate(R.layout.button, container);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED)
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            startActivityForResult(intent, 2);
+        });
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        LayoutInflater inflater = getLayoutInflater();
-        inflater.inflate(R.layout.button, (ViewGroup) findViewById(R.id.container));
-
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
     }
+
+
 }
